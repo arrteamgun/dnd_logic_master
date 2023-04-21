35b36986ca7a59a07604d493e0b9afe86c19f723
Roman Timganov <romatimganov@gmail.com>
2023-04-21 19:13:19 +0400

    new controller added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,20 @@
 using System;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
 
 
 public class HomeController : Controller
-{
-    
+{   
+    public string Index()
+    {
+        return "default action";
+    }
+
+    [HttpPost]
+    public IActionResult Result(DiceResult dice)
+    {
+        var rs = new RollService();
+        var result = rs.Result(dice.DiceType, dice.NumberOfDice, dice.Modifier);
+        return View(result.ToString()); //?
+    }
 }
--- Interfaces/IRollService.cs
+++ Interfaces/IRollService.cs
@@ -1,4 +1,5 @@
 public interface IRollService
+
 {
     public DiceResult Result(int diceType, int numberOfDice, int modifier);
 }
